b37f235faec2867e362d9cda35825d1b9d5299a7
JairFL27 <jairfb.27@gmail.com>
2020-11-28 21:49:04 -0500

    initial project

---
+++ src/main/java/com/backend/urp/BackendUrpApplication.java
@@ -0,0 +1,13 @@
+package com.backend.urp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendUrpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendUrpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/urp/BackendUrpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.urp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendUrpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
